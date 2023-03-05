df7164f83c49d098873b136304f100654a07c477
ryo8998 <ishino.r8998@gmail.com>
2023-03-05 22:21:17 +0900

    update main and tokenizer so that it can calculate with stack machine

--- rcc.c
+++ rcc.c
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+/* ---------------- Tyepdef宣言  -------------------*/
+
 // トークンの種類
 typedef enum{
     TK_RESERVED,    //記号
@@ -42,9 +44,20 @@ struct Token{
     char *str;          //トークン文字列
 };
 
+/* ----------------関数プロトタイプ宣言-----------------*/
+Node *primary();
+Node *expr();
+Node *mul();
+
+
+/* -----------------グローバル変数-------------------- */
+
 // 現在着目しているトークン
 Token *token;
 char* user_input;
+
+
+/* ---------------------関数------------------------*/
 
 // エラーを報告するための関数
 // printfと同じ引数を取る
@@ -121,7 +134,7 @@ Node *new_node(NodeKind kind, Node *lhs, Node *rhs){
 
 Node *new_node_num(int val){
     Node *node = calloc(1,sizeof(Node));
-    node->kidn = ND_NUM;
+    node->kind = ND_NUM;
     node->val = val;
     return node;
 }
@@ -132,18 +145,16 @@ mul = primary ("*" primary | "/" primary)*
 primary = num | "(" expr ")"
 */
 
-Node *expr(){
-    Node *node = mul();
-
-    for(;;){
-        if(consume('+')){
-            node = new_node(ND_ADD,node, mul());
-        }else if(consume('-')){
-            node = new_node(ND_SUB, node, mul());
-        }else{
-            reutun node;
-        }
+Node *primary(){
+    // 次のトークンが”("なら、 "(" expr ")"のはず
+    if(consume('(')) {
+        Node *node = expr();
+        consume(')');
+        return node;
     }
+
+    // そうでなければ数値のはず
+    return new_node_num(expect_number());
 }
 
 Node *mul(){
@@ -160,6 +171,52 @@ Node *mul(){
     }
 }
 
+Node *expr(){
+    Node *node = mul();
+
+    for(;;){
+        if(consume('+')){
+            node = new_node(ND_ADD,node, mul());
+        }else if(consume('-')){
+            node = new_node(ND_SUB, node, mul());
+        }else{
+            return node;
+        }
+    }
+}
+
+
+void gen(Node *node){
+    if(node->kind == ND_NUM){
+        printf("    push %d\n", node->val);
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("    pop rdi\n");
+    printf("    pop rax\n");
+
+    switch(node->kind){
+        case ND_ADD:
+            printf("    add rax, rdi\n");
+            break;
+        case ND_SUB:
+            printf("    sub rax, rdi\n");
+            break;
+        case ND_MUL:
+            printf("    imul rax, rdi\n");
+            break;
+        case ND_DIV:
+            printf("    cqo\n");
+            printf("    idiv rdi\n");
+            break;
+    }
+
+    printf("    push rax\n");
+}
+
 // 入力文字列pをトークナイズしてそれを返す
 Token *tokenize(char *p){
     Token head;
@@ -173,7 +230,7 @@ Token *tokenize(char *p){
             continue;
         }
 
-        if(*p == '+' || *p == '-'){
+        if(*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')'){
             cur = new_token(TK_RESERVED, cur, p++);
             continue;
         }
@@ -200,36 +257,22 @@ int main(int argc, char **argv){
         return 1;
     }
 
+    // トークナイズしてパースする
     user_input = argv[1];
-    //　トークナイズ
-    token = tokenize(argv[1]);
+    token = tokenize(user_input);
+    Node *node = expr();
 
     // アセンブリの前半部分を出力
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
 
-    // 式の最初は数でなければならないので、それをチェックして
-    // 最初のmov命令を出力
-    printf("    mov rax, %d\n", expect_number());
+    // 抽象構文木を降りながらコード生成
+    gen(node);
 
-
-    // '+<数>'あるいは'-<数>'というトークンの並びを消費しつつ
-    // アセンブリを出力
-
-    while(!at_eof()){
-        if(consume('+')){
-            printf("    add rax, %d\n", expect_number());
-            continue;
-        }
-
-        //オリジナルと異なる
-        if(consume('-')){
-            printf("    sub rax, %d\n", expect_number());
-            continue;
-        };
-    }
-
+    // スタックトップに式全体の値が残っているはずなので
+    // それをRAXにロードしてからの返り値とする
+    printf("    pop rax\n");
     printf("    ret\n");
     return 0;
 }
